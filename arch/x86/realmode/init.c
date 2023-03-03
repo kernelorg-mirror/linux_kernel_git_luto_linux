94f4dca4346fbccbe54b7cb376a8f95bd4050370
Andy Lutomirski <luto@kernel.org>
2023-03-02 16:00:19 -0800

    x86/boot: Improve realmode trampoline logging
    Log the location of the realmode trampoline, mostly so we can observe
    how subsequent patches move it.
    
    Signed-off-by: Andy Lutomirski <luto@kernel.org>

--- arch/x86/realmode/init.c
+++ arch/x86/realmode/init.c
@@ -55,10 +55,13 @@ void __init reserve_real_mode(void)
 
 	/* Has to be under 1M so we can execute real-mode AP code. */
 	mem = memblock_phys_alloc_range(size, PAGE_SIZE, 0, 1<<20);
-	if (!mem)
+	if (!mem) {
 		pr_info("No sub-1M memory is available for the trampoline\n");
-	else
+	} else {
 		set_real_mode_mem(mem);
+		pr_info("real mode trampoline is 0x%lx bytes at 0x%lx\n",
+			(unsigned long)size, (unsigned long)mem);
+	}
 }
 
 static void __init sme_sev_setup_real_mode(struct trampoline_header *th)
